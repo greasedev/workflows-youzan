fb1283283d06ad79049a28a509017b2b91c55c40
xl9211 <xulin97@gmail.com>
2026-05-01 22:23:58 +0800

    refactor(db): 将 stock 表的字段名从 stock 改为 store
    简化字段命名，避免表名与字段名相同造成的混淆，提高代码可读性。

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -6,7 +6,7 @@ export function initDB(agent: Agent) {
     report: "++id, &url",
     product:
       "++id, &barcode, status, createdTime, listedTime, transferredTime, returnedTime, listingRemindTime, transferRemindTime, returnRemindTime",
-    stock: "++id, &barcode, stock",
+    stock: "++id, barcode, store",
   });
   return db;
 }
